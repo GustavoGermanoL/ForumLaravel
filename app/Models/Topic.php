f40e757b0621a4433a81b0006ebfdc6d6144d580
GUSTAVO GERMANO LEMOS PEREIRA <0031432412025@lti.fatecso.edu.br>
2024-08-19 18:04:56 -0300

    19/08

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,7 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Topic extends Model
+
+class Topic extends Post
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this ->belongsToMany(Tag::class);
+    }
+
 }
